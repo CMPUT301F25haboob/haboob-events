a8e12bd2d99acfb72a0e91260a11cd0c3ee150e2
spencer-lukey <slukey@ualberta.ca>
2025-10-26 15:06:16 -0600

    Created Events class

---
+++ App/app/src/main/java/com/example/haboob/Event.java
@@ -0,0 +1,11 @@
+package com.example.haboob;
+
+public class Event {
+
+    // All of the lists that events have
+    private tags = new EventTagsList();
+    private entrants = new EntrantList();
+    private invitedUsers = new InviteList();
+    private enrolledUsers = new EnrolledList();
+    private cancelledUsers = new CancelledList();
+}
