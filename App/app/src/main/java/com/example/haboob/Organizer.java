617bf2a8b7ca1241ed900c2a5a04a2263d601ed5
dtyrrelldev <dtyrrell@ualberta.ca>
2025-11-02 15:55:58 -0700

    merged dev into my branch

--- App/app/src/main/java/com/example/haboob/Organizer.java
+++ App/app/src/main/java/com/example/haboob/Organizer.java
@@ -20,12 +20,12 @@ public class Organizer extends User {
         // SHOULD PULL FROM DATABASE TO SET EVENTSLIST
     }
 
-    // constructor that avoids Firestore for tests - Owen
-    public Organizer(String orgID, String firstName, String lastName, String email, String accountType, boolean inMemoryOnly) {
-        super(firstName, lastName, email, accountType);
-        this.organizerID = orgID;
-        this.events = new EventsList(inMemoryOnly);
-    }
+//    // constructor that avoids Firestore for tests - Owen
+//    public Organizer(String orgID, String firstName, String lastName, String email, String accountType, boolean inMemoryOnly) {
+//        super(firstName, lastName, email, accountType);
+//        this.organizerID = orgID;
+//        this.events = new EventsList(inMemoryOnly);
+//    }
 
     public EventsList getEventList() {
         return this.events;
