0090397eb54195417dba6eea21aee6c997309cb6
Owen Genge <owenxgenge@gmail.com>
2025-11-11 19:56:06 -0700

    Implemented the Notification class
    Represents a Notification object.

---
+++ App/app/src/main/java/com/example/haboob/Notification.java
@@ -0,0 +1,95 @@
+package com.example.haboob;
+import java.util.Date;
+import java.util.UUID;
+
+public class Notification {
+    public static final String DEFAULT_MESSAGE = "Empty Notification";
+
+    private String notificationId;
+    private String eventId;
+    private String organizerId; // sender ID
+    private String recipientId; // target device ID for entrant
+
+    private String message;
+    private boolean read;
+    private Date timeCreated;
+
+    // Empty constructor
+    public Notification() {
+        this.message = DEFAULT_MESSAGE;
+        this.read = false;
+        this.timeCreated = new Date();
+        this.notificationId = UUID.randomUUID().toString();
+    }
+
+    public Notification(String eventId, String organizerId, String recipientId, String message) {
+        this();
+        this.eventId = eventId;
+        this.organizerId = organizerId;
+        this.recipientId = recipientId;
+        setMessage(message);
+    }
+
+    // GETTERS
+    public String getNotificationId() {
+        return notificationId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public String getOrganizerId() {
+        return organizerId;
+    }
+
+    public String getRecipientId() {
+        return recipientId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public boolean isRead() {
+        return read;
+    }
+
+    public Date getTimeCreated() {
+        return timeCreated;
+    }
+
+    // SETTERS
+    public void setNotificationId(String notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public void setOrganizerId(String organizerId) {
+        this.organizerId = organizerId;
+    }
+
+    public void setRecipientId(String recipientId) {
+        this.recipientId = recipientId;
+    }
+
+    public void setMessage(String message) {
+        if (message == null || message.trim().isEmpty()) {
+            this.message = DEFAULT_MESSAGE;
+        } else {
+            this.message = message.trim();
+        }
+    }
+
+    public void setRead(boolean read) {
+        this.read = read;
+    }
+
+    public void setTimeCreated(Date timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+}
