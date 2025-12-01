1be70699cf1315af3b73d35f2cc5bea89339295d
Owen Genge <owenxgenge@gmail.com>
2025-11-30 19:14:03 -0700

    UI changes in organizer view to show selected event

--- App/app/src/main/java/com/example/haboob/EventAdapter.java
+++ App/app/src/main/java/com/example/haboob/EventAdapter.java
@@ -1,11 +1,14 @@
 package com.example.haboob;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
 
 import java.util.Date;
 import java.util.List;
@@ -15,6 +18,8 @@ import java.util.List;
  * Shows each event's title and marks events requiring a lottery with a red indicator.
  */
 public class EventAdapter extends ArrayAdapter<Event> {
+    private int selectedIndex = -1;
+
     /**
      * Creates an EventAdapter for displaying the given list of events.
      *
@@ -23,6 +28,10 @@ public class EventAdapter extends ArrayAdapter<Event> {
      */
     public EventAdapter(Context context, List<Event> events) {
         super(context, 0, events);
+    }
+
+    public void setSelectedIndex(int index) {
+        this.selectedIndex = index;
     }
 
     /**
@@ -35,10 +44,21 @@ public class EventAdapter extends ArrayAdapter<Event> {
 
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(
-                    android.R.layout.simple_list_item_1, parent, false);
+                    R.layout.organizer_event_item,  // CardView layout
+                    parent,
+                    false
+            );
         }
 
-        TextView textView = convertView.findViewById(android.R.id.text1);
+        CardView cardView = (CardView) convertView;
+        TextView textView = cardView.findViewById(R.id.tv_event_name);
+
+        // Highlight selected
+        if (position == selectedIndex) {
+            cardView.setCardBackgroundColor(Color.parseColor("#EBD7FF")); // light purple
+        } else {
+            cardView.setCardBackgroundColor(Color.WHITE); // default card color
+        }
 
         // Display the event name
         textView.setText(event.getEventTitle());
